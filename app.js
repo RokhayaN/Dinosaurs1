f53d4b34ed879d19ec996ab72d7c7f08bdbbcc8a
Dev_Rokhaya <rokhaya.codes@gmail.com>
2022-10-17 19:52:17 -0500

    Dinosaur objects created

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 
     // Create Dino Constructor
-
+    function Dinosaur(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.height = height;
+        this.weight = weight;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+      };
 
     // Create Dino Objects
+   const Triceratops = new Dinosaur("Triceratops", 13000, 114, "herbavor", "North America", "Late Cretaceous", "First discovered in 1889 by Othniel Charles Marsh");
+   const Tyrannosaurus_Rex = new Dinosaur("Tyrannosaurus Rex", 11905, 144, "carnivor", "North America", "Late Cretaceous","The largest known skull measures in at 5 feet long.");
+   const Anklyosaurus = new Dinosaur("Anklyosaurus", 10500, 55, "herbavor", "North America", "Late Cretaceous", "Anklyosaurus survived for approximately 135 million years.");
+   const Brachiosaurus = new Dinosaur("Brachiosaurus", 70000, 372, "herbavor", "North America", "Late Jurasic", "An asteroid was named 9954 Brachiosaurus in 1991.");
+   const Stegosaurus = new Dinosaur("Stegosaurus", 11600, 79, "herbavor", "North America, Europe, Asia", "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines.");
+   const Elasmosaurus = new Dinosaur("Elasmosaurus", 16000, 59, "carnivor", "North America", "Late Cretaceous", "Elasmosaurus was a marine reptile first discovered in Kansas.");
+   const Brachiosaurus = new Dinosaur();
+   const Brachiosaurus = new Dinosaur();
+   const Brachiosaurus = new Dinosaur();
+   
 
 
     // Create Human Object
